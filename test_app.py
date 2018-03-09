397aecd56209a280bfe8d82a9c4fac035d7e232a
David Bradway <dpb6@duke.edu>
2018-03-09 16:30:09 -0500

    add to tests

--- test_app.py
+++ test_app.py
@@ -6,6 +6,19 @@ endpoint : str = "/api/heart_rate"
 r = requests.post(ip+endpoint, json={"user_email": "suyash@suyashkumar.com",
                                      "user_age": 50, # in years
                                      "heart_rate": 100})
-res = r.json()
-print(res)
-print("The response was {0}.".format(res['result']))
+type(r)
+print(r.status_code)
+
+if r.status_code == 200:
+    res = r.json()
+    print(res)
+    print("The response was {0}.".format(res['result']))
+
+r = requests.get(ip+endpoint+"/suyash@suyashkumar.com")
+type(r)
+print(r.status_code)
+
+if r.status_code == 200:
+    res = r.json()
+    print(res)
+
