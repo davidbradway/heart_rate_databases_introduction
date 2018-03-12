77d89366b9c70b312193c4fd81b0914acafdd7b4
David Bradway <dpb6@duke.edu>
2018-03-12 14:27:38 -0400

    still broken

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from heart_rate_databases_starter import models
+from heart_rate_databases_starter import main
 import datetime
 
 from pymodm import connect
@@ -6,51 +8,58 @@ from pymodm import MongoModel, fields
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-connect("mongodb://localhost:27017/bme590") # connect to database
-
-class HeartRate(MongoModel):
-    user_email = fields.EmailField(primary_key=True)
-    user_age = fields.IntegerField()
-    heart_rate = fields.FloatField()
-    time = fields.DateTimeField()
+connect("mongodb://localhost:27017/heart_rate_app") # connect to database
 
 @app.route("/api/heart_rate", methods=["POST"])
 def set_heart_rate():
     r = request.get_json() # parses the POST request body as JSON
-    hr = HeartRate(user_email=r["user_email"],
-                   user_age=r["user_age"],
-                   heart_rate=r["heart_rate"],
-                   time=datetime.datetime.now())
-    hr.save()
+
+    user = helper_get_by_email(r["user_email"])
+    print(user)
+    print(type(user))
+
+    if user is None:
+        # create a new Users instance
+        u = heart_rate_databases_starter.models.User(email=r["user_email"],
+                                                     age=r["user_age"],
+                                                     heart_rate=[],
+                                                     heart_rate_times=[]) 
+    u.heart_rate.append(r["heart_rate"])
+    u.heart_rate_times.append(datetime.datetime.now())
+    u.save()
     return jsonify({"result": "saved"}), 200
 
 def helper_get_by_email(user_email):
     """
     Queries and returns all HearRate objects by given user_email
     """
-    return HeartRate.objects.raw({"user_email": user_email})
+    user = heart_rate_databases_starter.models.User.objects.raw({"_id": user_email}).first() # Get the first user where _id=email
+    return user
+
+def helper_get_users_average_heart_rate(user):
+    if len(user.heart_rate) > 0:
+        avg: float = sum(user.heart_rate)/len(user.heart_rate)
+    else:
+        avg = None
+    return avg
 
 @app.route("/api/heart_rate/<user_email>", methods=["GET"])
 def get_by_email(user_email):
     """
     Should return all heart rate measurements for that user
     """
-    heartrates = helper_get_by_email(user_email)
-    return jsonify(heartrates), 200
+    user = helper_get_by_email(user_email)
+    return jsonify(user.heart_rate), 200
 
 @app.route("/api/heart_rate/average/<user_email>", methods=["GET"])
 def get_average_by_email(user_email):
     """
     Should return the user's average heart rate over all measurements
     """
-    heartrates = helper_get_by_email(user_email)
-    total: float = 0.0
-    count: int = 0
-    for entry in heartrates:
-        total += entry.heart_rate
-        count += 1
-    if count != 0:
-        return jsonify({"average": total/count}), 200
+    user = helper_get_by_email(user_email)
+    avg = helper_get_users_average_heart_rate(user)
+    if avg is not None:
+        return jsonify({"average": avg}), 200
     else:
         return jsonify({"error": "no entries"}), 400
 
@@ -67,16 +76,12 @@ def get_average_by_email_since():
     heart_rate_average_since = r["heart_rate_average_since"] #"2018-03-09 11:00:36.372339" // date string
     since = datetime.strptime(heart_rate_average_since, "%Y-%m-%d %H:%M:%S.%f")
 
-    total: float = 0.0
-    count: int = 0
-    for entry in HeartRates.objects.raw({"user_email":user_email, "time": {'$gte': since}}):
-        total += entry.heart_rate
-        count += 1
-
     # TODO determine tachycardia for user_age
 
-    if count != 0:
-        return jsonify({"average": total/count}), 200
+    user =  heart_rate_databases_starter.models.User.objects.raw({"user_email":user_email, "time": {'$gte': since}}).first()
+    avg = helper_get_users_average_heart_rate(user)
+    if avg is not none:
+        return jsonify({"average": avg}), 200
     else:
         return jsonify({"error": "no entries"}), 400
 
